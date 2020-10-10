dd69b30b4aa5f6731fe6a1b094b274737d80e4f0
Sakti Dewantoro <saktyd@gmail.com>
2020-10-10 17:15:29 +0700

    add function show hide notification

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Notification() {
+    const [isShowNotif, setShowNotif] = useState(true)
+
+    const notifToggle = () => {
+        setShowNotif(!isShowNotif)
+    }
+
     return (
-        <div className="notification">
-            <div className="notification__content">
-                <span>By accessing and using this website, you acknowledge that you have read and
-                understand our Cookie Policy, Privacy Policy, and our Terms of Service.</span>
-                <button className="button__primary--blue">Got it</button>
-            </div>
+        <div>
+            { isShowNotif && ( 
+               <div className="notification">
+                <div className="notification__content">
+                    <span>By accessing and using this website, you acknowledge that you have read and
+                    understand our <a href="/">Cookie Policy, Privacy Policy,</a> and our <a href="/">Terms of Service.</a></span>
+                    <button className="button__primary--blue" onClick={notifToggle}>Got it</button>
+                </div>
+            </div> 
+             ) }
+            
         </div>
     )
 }
