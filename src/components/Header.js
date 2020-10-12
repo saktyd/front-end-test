e9ef0c625247d56e82a5795282ba214970fc3785
Sakti Dewantoro <saktyd@gmail.com>
2020-10-12 10:53:50 +0700

    rename header class name

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import logo from '../assets/image/y-logo-white.png'
 
 function Header() {
     return (
-        <div className="heroshot">
-            <img  className="heroshot__logo" src={logo} alt=""/>
-            <div className="heroshot__content">
-                <p className="heroshot__content--name">​Hello! I’m Sakti Dewantoro</p>
+        <div className="header">
+            <img  className="header__logo" src={logo} alt=""/>
+            <div className="header__content">
+                <p className="header__content--name">​Hello! I’m Sakti Dewantoro</p>
                 <h2>Consult, Design, and Develop Websites</h2>
-                <p className="heroshot__content--info">Have something great in mind? Feel free to contact me. <br/>
+                <p className="header__content--info">Have something great in mind? Feel free to contact me. <br/>
                 I'll help you to make it happen.</p>
                 <button className="button__transparent--blue"><h5>LET'S MAKE CONTACT</h5></button>
             </div>
